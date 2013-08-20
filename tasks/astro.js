5cc4a51597854cc840cd6dae941c93f331a21088
Matiboy <mathieu.chauvinc@gmail.com>
2013-08-20 16:15:51 +0800

    a

--- tasks/astro.js
+++ tasks/astro.js
@@ -9,6 +9,8 @@
 'use strict';
 
 module.exports = function(grunt) {
+  var htmlparser = require('htmlparser');
+  
   grunt.log.writeln('Start');
   // Please see the Grunt documentation for more information regarding task
   // creation: http://gruntjs.com/creating-tasks
@@ -23,8 +25,19 @@ module.exports = function(grunt) {
     // Iterate over all specified file groups.
     this.files.forEach(function(file) {
       var json = grunt.file.readJSON(file.src);
-      console.log(json);
-      grunt.log.writeln(json);
+      var htmlfile = file.src.replace('.json', '.html');
+      var rawHtml = grunt.file.read(htmlfile);
+      var handler = new htmlparser.DefaultHandler(function (error, dom) {
+        if (error)
+          console.log('ARGH!');
+        else
+          console.log(dom);
+      });
+      var parser = new htmlparser.Parser(handler);
+      parser.parseComplete(rawHtml);
+      if(json.footer) {
+
+      }
       return;
     });
   });

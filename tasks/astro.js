6593c5e413cd8022dd1a5246911e1893508be197
Matiboy <mathieu.chauvinc@gmail.com>
2013-08-20 15:39:49 +0800

    a

--- tasks/astro.js
+++ tasks/astro.js
@@ -9,7 +9,7 @@
 'use strict';
 
 module.exports = function(grunt) {
-
+  grunt.log('Start...');
   // Please see the Grunt documentation for more information regarding task
   // creation: http://gruntjs.com/creating-tasks
 
@@ -25,29 +25,6 @@ module.exports = function(grunt) {
       var json = grunt.file.readJSON(file);
       grunt.log(writeln('File') + file.src);
       return;
-      // Concat specified files.
-      var src = f.src.filter(function(filepath) {
-        // Warn on and remove invalid source files (if nonull was set).
-        if (!grunt.file.exists(filepath)) {
-          grunt.log.warn('Source file "' + filepath + '" not found.');
-          return false;
-        } else {
-          return true;
-        }
-      }).map(function(filepath) {
-        // Read file source.
-        return grunt.file.read(filepath);
-      }).join(grunt.util.normalizelf(options.separator));
-
-      // Handle options.
-      src += options.punctuation;
-
-      // Write the destination file.
-      grunt.file.write(f.dest, src);
-
-      // Print a success message.
-      grunt.log.writeln('File "' + f.dest + '" created.');
     });
   });
-
 };
